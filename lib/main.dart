dae0f3573ad03c11789504e6b3150c5c140dec86
spikerrbg <79044119+spikerrbg@users.noreply.github.com>
2024-12-21 09:16:21 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,22 @@
 import 'package:flangapp_pro/views/splashscreen.dart';
 import 'package:flutter/material.dart';
+import 'package:app_tracking_transparency/app_tracking_transparency.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Request App Tracking Transparency permission
+  await requestTrackingPermission();
+
   runApp(const MyApp());
+}
+
+// Function to request tracking permission
+Future<void> requestTrackingPermission() async {
+  final status = await AppTrackingTransparency.trackingAuthorizationStatus;
+  if (status == TrackingStatus.notDetermined) {
+    await AppTrackingTransparency.requestTrackingAuthorization();
+  }
 }
 
 class MyApp extends StatelessWidget {
